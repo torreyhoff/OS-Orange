88ab714c1f5ba9c481fa0f3c5b58125e84b4fb64
Torrey Hoff <torreyhoff@gmail.com>
2018-08-29 12:30:45 -0600

    causse I like to commit

--- orange/Plant.java
+++ orange/Plant.java
@@ -1,10 +1,21 @@
 package orange;
 
 public class Plant {
-
+	// How long do we want to run the juice processing
+	public static final long PROCESSING_TIME = 5 * 1000;
+	public final int ORANGES_PER_BOTTLE = 4;
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Startup a single plant - you put code here
+		
+		// plant processes Oranges until the time is up
+		
+		// Summarize the results at the end
+		// print total oranges started, processed, number of bottles finished and oranges
+		// wasted (not enough to fill a bottle)
 
 	}
+
+	// you write the plant functions you need
 
 }
