0f41885e9205b2514fba7be125157f3bd81a1988
Torrey Hoff <torreyhoff@gmail.com>
2018-08-29 12:19:24 -0600

    new files

---
+++ orange/Orange.java
@@ -0,0 +1,56 @@
+package orange;
+public class Orange {
+    public enum State {
+        Fetched(15),
+        Peeled(38),
+        Squeezed(29),
+        Bottled(17),
+        Processed(1);
+
+        private static final int finalIndex = State.values().length - 1;
+
+        final int timeToComplete;
+
+        State(int timeToComplete) {
+            this.timeToComplete = timeToComplete;
+        }
+
+        State getNext() {
+            int currIndex = this.ordinal();
+            if (currIndex >= finalIndex) {
+                throw new IllegalStateException("Already at final state");
+            }
+            return State.values()[currIndex + 1];
+        }
+    }
+
+    private State state;
+
+    public Orange() {
+        state = State.Fetched;
+        doWork();
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void runProcess() {
+        // Don't attempt to process an already completed orange
+        if (state == State.Processed) {
+            throw new IllegalStateException("This orange has already been processed");
+        }
+        doWork();
+        state = state.getNext();
+    }
+
+    private void doWork() {
+        // Sleep for the amount of time necessary to do the work
+        try {
+            Thread.sleep(state.timeToComplete);
+        } catch (InterruptedException e) {
+            System.err.println("Incomplete orange processing, juice may be bad");
+        }
+    }
+}
+// Thanks to Nate Williams for this code and the ideas
---
+++ orange/Plant.java
@@ -0,0 +1,10 @@
+package orange;
+
+public class Plant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
