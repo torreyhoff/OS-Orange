901d4ee513fb9dfc9191ca713b0db45d4b741c8f
Torrey Hoff <torreyhoff@gmail.com>
2018-09-10 13:03:48 -0600

    new file

---
+++ orange/Worker.java
@@ -0,0 +1,20 @@
+package orange;
+
+public class Worker implements Runnable {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	public Worker() {
+		Thread workerThread = new Thread();
+		workerThread.start();
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+
+}
