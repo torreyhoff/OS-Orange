8f732dbc3840f7bae505f2ccbc61a06e2d97ae78
Torrey Hoff <torreyhoff@gmail.com>
2018-09-14 10:03:14 -0600

    THreads on plant

--- orange/Plant.java
+++ orange/Plant.java
@@ -1,79 +1,73 @@
 package orange;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Plant extends Orange implements Runnable {
-	// How long do we want to run the juice processing
+
+	private static final int NUM_THREADS = 5;
 	public static final long PROCESSING_TIME = 5 * 1000;
 	public static final int ORANGES_PER_BOTTLE = 4;
 	public static final int WORKERS = 5;
-	public boolean timerDone = false;
-	private static int processedOrange = 0;
-	private static int unProcessedOrange = 0;
+	public static boolean orangeGoneBad = false;
+	public static int counter = 0;
+	public static int badOranges = 0;
+	public static long startTime = System.currentTimeMillis();
+	
+	private final String name;
+	private final Thread thread;
 	 
 	public static void main(String[] args) {
 		// Startup a single plant - you put code here
-		Plant plant = new Plant();
-		
-		// plant processes Oranges until the time is up
-		
-		timePlant(plant);
-		plant.Worker();
-		
-		// Summarize the results at the end
-		// print total oranges started, processed, number of bottles finished and oranges
-		// wasted (not enough to fill a bottle)
-		System.out.println("Oranges Started: "+(processedOrange+unProcessedOrange));
-		System.out.println("Oranges Processed: "+processedOrange);
-		System.out.println("Bottles Finished: "+processedOrange/ORANGES_PER_BOTTLE);
-		System.out.println("Oranges Wasted: "+(processedOrange%ORANGES_PER_BOTTLE+unProcessedOrange));		
-		System.exit(0);
+		Plant[] plant = new Plant[NUM_THREADS];
+		String nameOfOrange = "Orange/Thread ";
+		for (int i = 0; i < NUM_THREADS; i++){
+			plant[i]= new Plant(nameOfOrange + i); 
+		}
+		for (int j = 0; j < NUM_THREADS; j++) {
+			try {
+				System.out.println(j + " is going to die");
+				plant[j].getThread().join();
+				System.out.println(j + " died");
+			} catch (InterruptedException ignored) {}
+		}
+		if (!orangeGoneBad) {
+			System.out.printf("\n%d Oranges were processed\n%d Bottles were made\n%d Oranges were wasted\n", counter,
+					counter / ORANGES_PER_BOTTLE, counter % ORANGES_PER_BOTTLE);
+		} else {
+			System.out.printf("\n%d Oranges were processed\n%d Bottles were made\n%d Oranges were wasted\n", counter,
+					counter / ORANGES_PER_BOTTLE, counter % ORANGES_PER_BOTTLE +badOranges);
+		}
 
 	}
 
 	// you write the plant functions you need
-	public Plant() {
+	public Plant(String name) {
 		System.out.println("Creating Orange Plantation.");
-		this.timerDone = false;
-		
+		this.name = name;
+		thread = new Thread(this);
+		thread.start();
 	}
 	
-	private void processOranges() {
-		Orange currentOrange = new Orange();
-		if(currentOrange.getState() == Orange.State.Processed) {
-			return;
-		}
-		else {
+	public Thread getThread() {
+		return thread;
+	}
+	
+	private void processOranges(Orange currentOrange) {
+		while (currentOrange.getState() != Orange.State.Processed && (System.currentTimeMillis() - startTime) < PROCESSING_TIME) {
 			currentOrange.runProcess();
 		}
-	}
-	
-	private static void timePlant(Plant plant) {
-		Timer timeThoseOranges = new Timer("threadOne");
-		TimerTask taskTimerForOranges = new TimerTask() {
-			public void run() {
-				plant.timerDone = true;
-			}
-		};
-		timeThoseOranges.schedule(taskTimerForOranges, PROCESSING_TIME);
-		
-	}
-	
-	public void Worker() {
-		Thread workerThread = new Thread(this);
-		workerThread.start();
+		if (currentOrange.getState() != Orange.State.Processed){
+			orangeGoneBad=true;
+			badOranges++;
+		}
 	}
 	
 	@Override
 	public void run() {
-		while(!this.timerDone) {
-			this.processOranges();
-			if(this.timerDone) {
-				unProcessedOrange++;
-				break;
-			} else {
-				processedOrange++;
+		System.out.println("Starting thread " + name);
+		while ((System.currentTimeMillis() - startTime) < PROCESSING_TIME) {
+			Orange o = new Orange();
+			processOranges(o);
+			if (!orangeGoneBad) {
+				counter++;
 			}
 		}
 	}
